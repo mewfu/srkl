5f3975b22834eeb18184ea497d2ffcacea90942a
mewfu <150902459+mewfu@users.noreply.github.com>
2023-11-18 12:32:47 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||卢|晓冰|祝你|生日|快乐|#rectangle|');
+      S.UI.simulate('|#countdown 3||卢|晓冰|祝你|生日|快乐|⭐|');
     }
 
     S.Drawing.loop(function () {
@@ -206,7 +206,7 @@ S.UI = (function () {
         default:
           S.Shape.switchShape(S.ShapeBuilder.letter(current[0] === cmd ? 'What?' : current));
       }
-    },3500, sequence.length);
+    },3000, sequence.length);
   }
 
   function checkInputWidth(e) {
